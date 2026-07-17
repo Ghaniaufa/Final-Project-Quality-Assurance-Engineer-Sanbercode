8220b78fb7bf56ab11fee81763f93a63ced53850
Ghaniaufa <maulaghani.alaufa@gmail.com>
2026-07-17 16:20:35 +0700

    Create Data for LoginPage

---
+++ cypress/Pages/LoginPage.js
@@ -0,0 +1,49 @@
+class LoginPage {
+
+    visit(){
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    }
+
+   inputUsername(username){
+
+    cy.get('input[name="username"]').clear()
+
+    if(username){
+        cy.get('input[name="username"]').type(username)
+    }
+
+    }
+
+    inputPassword(password){
+
+    cy.get('input[name="password"]').clear()
+
+    if(password){
+        cy.get('input[name="password"]').type(password)
+    }
+
+}
+
+    clickLogin(){
+
+        cy.get('button[type="submit"]').click()
+
+    }
+
+    verifyDashboard(){
+
+        cy.url().should('include','dashboard')
+
+    }
+
+    verifyInvalidCredential(){
+
+        cy.contains('Invalid credentials').should('be.visible')
+
+    }
+
+}
+
+export default LoginPage
