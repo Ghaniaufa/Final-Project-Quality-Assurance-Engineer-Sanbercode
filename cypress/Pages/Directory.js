54150a8cd3495af96eea44bb6cb089b86e143ad3
Ghaniaufa <maulaghani.alaufa@gmail.com>
2026-07-17 20:05:12 +0700

    Fix the script method for a lot requested time

--- cypress/Pages/Directory.js
+++ cypress/Pages/Directory.js
@@ -1,46 +1,58 @@
 class DirectoryPage {
 
-    menuDirectory(){
+    menuDirectory() {
         cy.contains('Directory').click()
+
+        cy.url({ timeout: 20000 }).should('include', '/directory')
+
+        cy.get('input[placeholder*="Type for hints"]', {
+            timeout: 20000
+        }).should('be.visible')
     }
 
-    verifyDirectoryPage(){
-        cy.url().should('include','directory')
-        cy.contains('Directory').should('be.visible')
+    verifyDirectoryPage() {
+        cy.url().should('include', '/directory')
     }
 
-    inputEmployee(name){
-        cy.get('input[placeholder="Type for hints..."]')
+    inputEmployee(name) {
+        cy.get('input[placeholder*="Type for hints"]')
+            .should('be.visible')
             .clear()
             .type(name)
     }
 
-    selectJobTitle(title){
+    selectJobTitle(title) {
         cy.get('.oxd-select-text').eq(0).click()
-        cy.contains(title).click()
+        cy.contains('.oxd-select-option', title).click()
     }
 
-    selectLocation(location){
+    selectLocation(location) {
         cy.get('.oxd-select-text').eq(1).click()
-        cy.contains(location).click()
+        cy.contains('.oxd-select-option', location).click()
     }
 
-    clickSearch(){
-        cy.contains('button','Search').click()
-    }
-
-    clickReset(){
-        cy.contains('button','Reset').click()
-    }
-
-    verifyResultExist(){
-        cy.get('.oxd-sheet')
+    clickSearch() {
+        cy.contains('button', 'Search')
             .should('be.visible')
+            .click()
     }
 
-    verifyNoRecord(){
-        cy.contains('No Records Found')
+    clickReset() {
+        cy.contains('button', 'Reset')
             .should('be.visible')
+            .click()
+    }
+
+    verifyResultExist() {
+        cy.get('.oxd-sheet', {
+            timeout: 20000
+        }).should('be.visible')
+    }
+
+    verifyNoRecord() {
+        cy.contains('No Records Found', {
+            timeout: 20000
+        }).should('be.visible')
     }
 
 }
