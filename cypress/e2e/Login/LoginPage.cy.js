3a9a7e7cb295f309cd12967a98ee57e7ff7d620c
Ghaniaufa <maulaghani.alaufa@gmail.com>
2026-07-17 16:16:36 +0700

    first commit

---
+++ cypress/e2e/Login/LoginPage.cy.js
@@ -0,0 +1,93 @@
+import LoginPage from '../../pages/LoginPage.js'
+import data from '../../fixtures/loginData.json'
+
+const login = new LoginPage()
+
+describe('OrangeHRM Login POM', () => {
+    //TC001
+    it('TC001 - Login Success', () => {
+
+        login.visit()
+        login.inputUsername(data.validUser)
+        login.inputPassword(data.validPassword)
+        login.clickLogin()
+        login.verifyDashboard()
+
+    })
+    //TC002
+    it('TC002 Empty Username',()=>{
+
+        login.visit()
+        login.inputUsername(data.empty)
+        login.inputPassword(data.validPassword)
+        login.clickLogin()
+        cy.contains('Required').should('be.visible')
+
+    })
+    //TC003
+    it('TC003 Empty Password',() => {
+
+        login.visit()
+        login.inputUsername(data.validPassword)
+        login.inputPassword(data.empty)
+        login.clickLogin()
+        cy.contains('Required').should('be.visible')
+    })
+    //TC004
+    it('TC004 Empty Username and Password',() => {
+
+        login.visit()
+        login.inputUsername(data.empty)
+        login.inputPassword(data.empty)
+        login.clickLogin()
+        cy.contains('Required').should('be.visible')
+    })
+    //TC005
+    it('TC005 Invalid Username',()=>{
+
+    login.visit()
+    login.inputUsername(data.wrongUser)
+    login.inputPassword(data.validPassword)
+    login.clickLogin()
+    login.verifyInvalidCredential()
+
+    })
+    //TC006
+    it('TC006 Invalid Password',()=>{
+
+    login.visit()
+    login.inputUsername(data.validUser)
+    login.inputPassword(data.wrongPassword)
+    login.clickLogin()
+    login.verifyInvalidCredential()
+
+    })
+    //TC007
+    it('TC007 SQL Injection',()=>{
+
+    login.visit()
+
+    login.inputUsername(data.sqlInjection)
+    login.inputPassword(data.sqlInjection)
+    login.clickLogin()
+    login.verifyInvalidCredential()
+
+    })
+    //TC008
+    it('TC008 Username Space', () => {
+
+    login.visit()
+
+    login.inputUsername(' Admin')
+    login.inputPassword(data.validPassword)
+
+    login.clickLogin()
+
+    cy.contains('Invalid credentials').should('be.visible')
+
+    cy.url().should('include','/auth/login')
+
+})
+
+})
+
